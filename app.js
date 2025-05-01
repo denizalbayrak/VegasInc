08aeb825c904fc416bce364023884cc3a6f54249
denizalbayrak <deniz.al26@gmail.com>
2025-05-01 17:02:52 +0300

    Update

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
-
 // ********** set date ************
 // select span
 const date = document.getElementById("date");
@@ -82,3 +77,5 @@ scrollLinks.forEach((link) => {
   });
 });
 // calculate heights
+
+const letsEatBtn = document.get
